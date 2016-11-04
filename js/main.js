b631652f32ca489feb60501235c76104a2abe220
s1hsgw <shsgwixdjs@gmail.com>
2016-11-04 20:52:47 +0900

    added animation functions for each level

--- js/main.js
+++ js/main.js
@@ -9,78 +9,111 @@
     backBar = animatingSvg.select('#back-bar'),
     seatBase = animatingSvg.select('#seat-base'),
     seatBottom = animatingSvg.select('#seat-bottom'),
-    cylinderBase = animatingSvg.select('#cylinder-base'),
-    cylinderBottom = animatingSvg.select('#cylinder-bottom'),
+    legBase = animatingSvg.select('#leg-base'),
+    legBottom = animatingSvg.select('#leg-bottom'),
+    legBase2 = animatingSvg.select('#leg-base2'),
+    legBottom2 = animatingSvg.select('#leg-bottom2'),
     floor = animatingSvg.select('#floor');
 
+  var isLv3 = false,
+      isSwinging = false;
+
+
+  /* ----------------------------
+
+  Status Test
+
+  ---------------------------- */
+
   var startBtn = document.querySelector('#start');
-  var upliftBtn = document.querySelector('#uplift');
-  var downshiftBtn = document.querySelector('#downshift');
+  var lv1Btn = document.querySelector('#lv1');
+  var lv2Btn = document.querySelector('#lv2');
+  var lv3Btn = document.querySelector('#lv3');
+  var restartBtn = document.querySelector('#restart');
   var updownBtn = document.querySelector('#updown');
 
   startBtn.addEventListener('click', function() {
-    animateFloor();
+    initAnimation();
+  });
+
+  lv1Btn.addEventListener('click', function() {
+    animateLv1();
+  });
+
+  lv2Btn.addEventListener('click', function() {
+    animateLv2();
+  });
+
+  lv3Btn.addEventListener('click', function() {
+    animateLv3();
+  });
+
+  restartBtn.addEventListener('click', function() {
+    restart();
+  });
+
+
+  /* ----------------------------
+
+  Init Animation Functions
+
+  ---------------------------- */
+
+  function initAnimation() {
+    initFloor();
     $('#animated-svg').addClass('lv0');
-  });
+  }
 
-  upliftBtn.addEventListener('click', function() {
-    uplift();
-    $('#animated-svg').removeClass('lv0');
-    $('#animated-svg').addClass('lv3');
-  });
-
-  downshiftBtn.addEventListener('click', function() {
-    downshift();
-    $('#animated-svg').removeClass('lv3');
-    $('#animated-svg').addClass('lv0');
-  });
-
-
-  function animateFloor() {
+  function initFloor() {
     setTimeout(function() {
       floor.animate({
         'x2': floor.attr('data-x')
-      }, 400, mina.easeinout, animateCylinder);
+      }, 400, mina.easeinout, initleg);
     }, 400);
   }
 
-  function animateCylinder() {
+  function initleg() {
     setTimeout(function() {
-      cylinderBottom.animate({
-        'height': cylinderBottom.attr('data-height')
+      legBottom.animate({
+        'height': legBottom.attr('data-height')
+      }, 400, mina.elastic);
+      legBottom2.animate({
+        'height': legBottom2.attr('data-height')
       }, 400, mina.elastic);
     }, 200);
 
     setTimeout(function() {
-      cylinderBase.animate({
-        'height': cylinderBase.attr('data-height')
-      }, 400, mina.elastic, animateSeat);
+      legBase.animate({
+        'height': legBase.attr('data-height')
+      }, 400, mina.elastic, initSeat);
+      legBase2.animate({
+        'height': legBase2.attr('data-height')
+      }, 400, mina.elastic, initSeat);
     }, 100);
   }
 
-  function animateSeat() {
+  function initSeat() {
     seatBase.animate({
       'width': seatBase.attr('data-width')
     }, 300, mina.easeinout);
 
     seatBottom.animate({
       'x2': seatBottom.attr('data-x')
-    }, 300, mina.easeinout, animateBack);
+    }, 300, mina.easeinout, initBack);
   }
 
-  function animateBack() {
+  function initBack() {
     backBar.animate({
       'height': backBar.attr('data-height')
     }, 800, mina.elastic);
     setTimeout(function() {
       backBase.animate({
         'height': backBase.attr('data-height')
-      }, 800, mina.elastic, animateFace);
+      }, 800, mina.elastic, initFace);
     }, 200);
   }
 
-
-  function animateFace() {
+  function initFace() {
     rightEye.animate({
       transform: 's1 1'
     }, 400, mina.easeinout);
@@ -91,90 +124,173 @@
     setTimeout(function() {
       mouse.animate({
         transform: 's1 1'
-      }, 400, mina.easeinout, function() {
-        // $('#animated-svg').addClass('lv1');
-      });
+      }, 400, mina.easeinout);
     }, 300);
   }
 
+  /* ----------------------------
 
+  Level Animation Functions
+
+  ---------------------------- */
+
+  function animateLv0() {
+    closeMouse();
+    $('#animated-svg').removeClass('lv3');
+    $('#animated-svg').addClass('lv0');
+  }
+
+  function animateLv1() {
+    lookRight();
+    $('#animated-svg').removeClass('lv0');
+    $('#animated-svg').addClass('lv1');
+  }
+
+  function animateLv2() {
+    openMouse();
+    $('#animated-svg').removeClass('lv1');
+    $('#animated-svg').addClass('lv2');
+  }
+
+  function animateLv3() {
+    resetFacePosition();
+    $('#animated-svg').removeClass('lv2');
+    $('#animated-svg').addClass('lv3');
+  }
+
+  function openMouse() {
+    mouse.animate({
+      'height': 15
+    }, 600, mina.elastic);
+  }
+
+  function closeMouse() {
+    mouse.animate({
+      'height': 2
+    }, 600, mina.elastic);
+  }
+
+  function restart() {
+    isLv3 = false;
+    animateLv0();
+  }
 
   function uplift() {
-    cylinderBase.animate({
-      'height': 44
-    }, 600, mina.elastic);
+
+    isLv3 = true;
+
+    legBase.animate({
+      'height': 29
+    }, 75, mina.elastic);
+    legBase2.animate({
+      'height': 29
+    }, 75, mina.elastic);
     seatBase.animate({
-      transform: 'translate(0, -20)'
-    }, 600, mina.elastic);
+      transform: 'translate(0, -15)'
+    }, 75, mina.elastic);
     seatBottom.animate({
-      transform: 'translate(0, -20)'
-    }, 600, mina.elastic);
+      transform: 'translate(0, -15)'
+    }, 75, mina.elastic);
     leftEye.animate({
-      transform: 'translate(0, -20)'
-    }, 600, mina.elastic);
+      transform: 'translate(0, -15)'
+    }, 75, mina.elastic);
     rightEye.animate({
-      transform: 'translate(0, -20)'
-    }, 600, mina.elastic);
+      transform: 'translate(0, -15)'
+    }, 75, mina.elastic);
     mouse.animate({
-      transform: 'translate(0, -20)'
-    }, 600, mina.elastic);
+      transform: 'translate(0, -15)'
+    }, 75, mina.elastic);
     backBase.animate({
-      transform: 'rotate(180 99 40) translate(0, 20)'
-    }, 600, mina.elastic);
+      transform: 'rotate(180 99 40) translate(0, 15)'
+    }, 75, mina.elastic);
     backBar.animate({
-      transform: 'rotate(180 98 80) translate(0, 20)'
-    }, 600, mina.elastic);
+      transform: 'rotate(180 98 80) translate(0, 15)'
+    }, 75, mina.elastic, function() {
+      downshift();
+    });
   }
 
   function downshift() {
-    cylinderBase.animate({
+    legBase.animate({
       'height': 24
-    }, 400, mina.elastic);
+    }, 75, mina.elastic);
+    legBase2.animate({
+      'height': 24
+    }, 75, mina.elastic);
     seatBase.animate({
       transform: 'translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     seatBottom.animate({
       transform: 'translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     leftEye.animate({
       transform: 'translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     rightEye.animate({
       transform: 'translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     mouse.animate({
       transform: 'translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     backBase.animate({
       transform: 'rotate(180 99 40) translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic);
     backBar.animate({
       transform: 'rotate(180 98 80) translate(0, 0)'
-    }, 600, mina.elastic);
+    }, 75, mina.elastic, function() {
+      if (isLv3) uplift();
+    });
 
   }
 
+  function lookRight() {
 
-  function showClouds() {
-    clouds1.animate({
-      transform: 't210 0'
-    }, 12000);
-    clouds2.animate({
-      transform: 't-210 0'
-    }, 12000, function() {
-      hideClouds();
+    isSwinging = true;
+
+    mouse.animate({
+      transform: 't4 0'
+    }, 1200, mina.easeinout);
+    rightEye.animate({
+      transform: 't4 0'
+    }, 1200, mina.easeinout);
+    leftEye.animate({
+      transform: 't6 0'
+    }, 1200, mina.easeinout, function() {
+      lookLeft();
     });
   }
 
-  function hideClouds() {
-    clouds1.animate({
-      transform: 't-80 0'
-    }, 12000);
-    clouds2.animate({
-      transform: 't70 0'
-    }, 12000, function() {
-      showClouds();
+  function lookLeft() {
+
+    mouse.animate({
+      transform: 't-3 0'
+    }, 1200, mina.easeinout);
+    rightEye.animate({
+      transform: 't-5 0'
+    }, 1200, mina.easeinout);
+    leftEye.animate({
+      transform: 't-3 0'
+    }, 1200, mina.easeinout, function() {
+      if (isSwinging) lookRight();
     });
   }
+
+  function resetFacePosition() {
+
+    isSwinging = false;
+
+    mouse.animate({
+      transform: 't0 0'
+    }, 600, mina.easeinout);
+    rightEye.animate({
+      transform: 't0 0'
+    }, 600, mina.easeinout);
+    leftEye.animate({
+      transform: 't0 0'
+    }, 600, mina.easeinout, function() {
+      uplift();
+    });
+  }
+
 
 }());
